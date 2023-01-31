0916abc8089eb16ac269f40dc8c97a2cb161c7bc
sumitverma770 <123858125+sumitverma770@users.noreply.github.com>
2023-01-31 17:12:53 +0000

    Add files via upload

---
+++ si.py
@@ -0,0 +1,10 @@
+# si = p*r*t/100
+p = int(input('principle:'))
+r = float(input('rate:'))
+t = int(input('time:'))
+si= (p*r*t)/100
+print('simple intrest calc')
+print(f'{p=}')
+print(f'{r=}')
+print(f'{t=}')
+print(f'simple intrest:{si}')
