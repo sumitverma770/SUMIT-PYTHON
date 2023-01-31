fb62ce84d9d82088d25eab6c61c1931daa826287
Your Name <you@example.com>
2023-01-31 16:42:19 +0000

    operator

---
+++ ar.py
@@ -0,0 +1,7 @@
+d= int(input("enter diameter of circle:"))
+r=d/2
+area= 3.14*r**2
+circ=2*3.14*r
+print("f'diameter of circle:{d}")
+print("f'area of circle:{area}")
+print("f'circumference of circle:{circ}")
--- sumit.py
+++
@@ -1,6 +0,0 @@
-import math as M  
-Diameter = float (input ("Please enter the radius of the given circle: "))  
-area_of_the_circle = M.pi* diameter/2 * diameter/2
-circumference=2*3.14*diameter/2
-print (circumference of circle:{circumference}
-print (" The area of the given circle is: ", area_of_the_circle)  
