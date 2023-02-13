9fd80e0b71d8ded11216a3f2d4c1abca30d9980c
Your Name <you@example.com>
2023-02-13 09:57:49 +0000

    function

---
+++ t1.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+def square(d):
+    for i in range(4):
+        fd(d)
+        lt(90)
+
+def polygon(side=3,dis=50):
+    pass
+
+
+polygon(5,100)
+polygon(6,98)
+square(100)
+square(500)
+hideturtle()
+mainloop()
